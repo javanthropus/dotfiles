66ffa917cb3fc88f9448b9ad187b191b2a24ea0a
Jeremy Bopp <jeremy@bopp.net>
2019-04-15 08:33:21 -0500

    Remove support for using gvim as the manpager

--- bin/manpager.sh
+++ bin/manpager.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-if type gvim >&/dev/null; then
-	col -b | gvim -R -c 'set ft=man nomod nolist' - 2>/dev/null
-elif type vim >&/dev/null; then
+if type vim >&/dev/null; then
 	col -b | vim -R -c 'set ft=man nomod nolist' -
 else
 	less
