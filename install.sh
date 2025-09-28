a9f0a5c95e067b3ef2b4c0348cae292ab0ba01a6
Jeremy Bopp <jeremy@bopp.net>
2025-09-27 23:20:56 -0500

    Fix formatting for the install script

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# vim: noet ts=2 sw=2:
 
 set -e
 
@@ -6,9 +7,9 @@ DOTFILES_DIR=$(cd "$(dirname $0)" && pwd)
 
 cd "$DOTFILES_DIR"
 find .* * -mindepth 0 -maxdepth 0 \
-       ! -name . ! -name .. \
-       ! -name .git \
-       ! -name .gitignore \
-       ! -name install.sh \
-       ! -name .\*.sw\? \
-       -exec ln -snf "$DOTFILES_DIR/{}" ~/{} \;
+	! -name . ! -name .. \
+	! -name .git \
+	! -name .gitignore \
+	! -name install.sh \
+	! -name .\*.sw\? \
+	-exec ln -snf "$DOTFILES_DIR/{}" ~/{} \;
